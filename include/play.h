87cdb0b6355e2a0abcade41f998f08efa8dfba7a
Lily Ehsani <lilyehsani@uchicago.edu>
2021-05-05 21:01:47 -0500

    Updated play.h

--- include/play.h
+++ include/play.h
@@ -11,4 +11,15 @@
 /* do_turn: executes a single turn based on given game and user input */
 int do_turn(game* g);
 
+/* print_outcome: prints an outcome message given an outcome */
+void print_outcome(outcome o);
+
+/* change_turn: changes the turn within the given game struct */
+turn change_turn(game* g);
+
+/* play_game: executes an entire game from start to finish given a
+ * new game */
+void play_game(game* g);
+
+
 #endif /* PLAY_H */
